47ffff6023f53af8939e1f2ec0e7d3ed2749270c
theDreamBear <512068167@qq.com>
2020-01-10 20:48:09 +0800

    add

---
+++ leetcode/204.计数质数.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=cpp
+ *
+ * [204] 计数质数
+ *
+ * https://leetcode-cn.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Easy (31.42%)
+ * Likes:    255
+ * Dislikes: 0
+ * Total Accepted:    37.9K
+ * Total Submissions: 120.1K
+ * Testcase Example:  '10'
+ *
+ * 统计所有小于非负整数 n 的质数的数量。
+ * 
+ * 示例:
+ * 
+ * 输入: 10
+ * 输出: 4
+ * 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countPrimes(int n) {
+        // 暴力算出所有
+        
+        // 埃氏筛学法
+    }
+};
+// @lc code=end
+
