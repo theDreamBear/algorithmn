40d7f5646718e3b18a0928090e750e5b79ce530a
theDreamBear <512068167@qq.com>
2020-10-20 14:42:01 +0800

    0

--- leetcode/215.数组中的第k个最大元素.cpp
+++ leetcode/215.数组中的第k个最大元素.cpp
@@ -47,6 +47,49 @@ class Solution {
         return findKthLargestHelper(0, nums.size() - 1, nums, k);
     }
 
-    int findKthLargest(vector<int>& nums, int k) { return -1; }
+    void merge(int low, int mid, int high, vector<int>& nums) {
+        int temp[high - low + 1];
+        int i = low, j = mid + 1, idx = 0;
+        while (i <= mid && j <= high) {
+            if (nums[i] < nums[j]) {
+                temp[idx++] = nums[j++];
+            } else {
+                temp[idx++] = nums[i++];
+            }
+        }
+        while (i <= mid) {
+            temp[idx++] = nums[i++];
+        }
+        while (j <= high) {
+            temp[idx++] = nums[j++];
+        }
+        for (int i = low; i <= high; ++i) {
+            nums[i] = temp[i - low];
+        }
+        return;
+    }
+
+    /*
+        归并排序
+    */
+    void divideAndConquer(int low, int high, vector<int>& nums) {
+        if (low >= high) {
+            return;
+        }
+        if (low + 1 == high) {
+            if (nums[low] < nums[high]) {
+                swap(nums[low], nums[high]);
+            }
+        }
+        int mid = (low + high) / 2;
+        divideAndConquer(low, mid, nums);
+        divideAndConquer(mid + 1, high, nums);
+        merge(low, mid, high, nums);
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        divideAndConquer(0, nums.size() - 1, nums);
+        return nums[k - 1];
+    }
 };
 // @lc code=end
