0037cded0f1b6272203f126eb7c54dc02cab1c9b
theDreamBear <512068167@qq.com>
2020-12-09 14:54:59 +0800

    0

--- leetcode/80.删除排序数组中的重复项-ii.cpp
+++ leetcode/80.删除排序数组中的重复项-ii.cpp
@@ -3,26 +3,27 @@
  *
  * [80] 删除排序数组中的重复项 II
  */
-#include <iostream>
-#include <string>
+#include <ranges.h>
 #include <string.h>
-#include <vector>
-#include <utility>
+
+#include <algorithm>
+#include <iostream>
 #include <map>
+#include <queue>
 #include <set>
 #include <stack>
-#include <queue>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <algorithm>
-#include <ranges.h>
+#include <utility>
+#include <vector>
 
 using namespace std;
 // @lc code=start
 class Solution {
-public:
+ public:
     void leftRotate(vector<int>& nums, int low, int high, int k) {
-        for (int i = low, j = low +  k - 1; i < j; ++i, --j) {
+        for (int i = low, j = low + k - 1; i < j; ++i, --j) {
             swap(nums[i], nums[j]);
         }
         for (int i = low + k, j = high; i < j; ++i, --j) {
@@ -51,7 +52,7 @@ public:
                     int k = right - left - 1;
                     ans -= k;
                     if (k > 0) {
-                        leftRotate(nums,left, nums.size() - 1, k);
+                        leftRotate(nums, left, nums.size() - 1, k);
                     }
                     left = i - k;
                     right = -1;
@@ -62,7 +63,6 @@ public:
                     right = -1;
                     ++i;
                 }
-
             }
         }
         if (right - left > 1) {
@@ -104,15 +104,15 @@ public:
         return ans;
     }
 
-    // [0, j] 已处理区间
-    // (i, len) 待处理区间
+    // [0, j) 区间的数是有效的
+    //
 
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates_3(vector<int>& nums) {
         if (nums.size() <= 1) {
             return nums.size();
         }
         int j = 1, count = 1;
-        for (int i = 1; i < nums.size();++i) {
+        for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] == nums[i - 1]) {
                 ++count;
             } else {
@@ -124,11 +124,50 @@ public:
         }
         return j;
     }
+
+    // [0, j) 区间的数是有效的
+    // [j, i) 待覆盖区间
+
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0, count = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (count == 0) {
+                ++j;
+                ++count;
+                continue;
+            }
+            if (nums[i] == nums[i - 1]) {
+                ++count;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+
+    int removeDuplicates_4(std::vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int L = 0;
+        for (int R = 0; R < nums.size(); ++R) {
+            // nums[L-2] < nums[R]
+            //    如果重复数字个数不大于2，那么这个判断肯定是成立的
+            //    如果重复个数大于2，那么nums[L-2] < nums[R]就是不成立的
+            //      即 多余重复项都被忽视了，直到遇到新的数字或者数字结束
+            if (L < 2 || nums[L - 2] < nums[R]) {
+                nums[L] = nums[R];
+                ++L;
+            }
+        }
+        return L;
+    }
 };
 // @lc code=end
 
 int main() {
-    vector<int> ans = {0,0,0,0,0,1,2,2,3,3,4,4};
+    vector<int> ans = {0, 0, 0, 0, 0, 1, 2, 2, 3, 3, 4, 4};
     int len = Solution{}.removeDuplicates(ans);
     for (int i = 0; i < len; ++i) {
         cout << ans[i] << " ";
