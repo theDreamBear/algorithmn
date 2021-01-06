f53616df0dbd579088b59b475e10131971bf30d3
theDreamBear <512068167@qq.com>
2021-01-06 12:28:51 +0800

    0

---
+++ leetcode/485.最大连续-1-的个数.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=485 lang=cpp
+ *
+ * [485] 最大连续1的个数
+ */
+#include <iostream>
+#include <utility>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0;
+        int left = 0, right = 0;
+        while (right < nums.size()) {
+            // 过滤0
+            while (right < nums.size() && nums[right] == 0) {
+                ++right;
+            }
+            left = right;
+            // 计算1
+            while (right < nums.size() && nums[right] == 1) {
+                ++right;
+            }
+            int len = right - left;
+            if (len > ans) {
+                ans = len;
+            }
+            left = right;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
