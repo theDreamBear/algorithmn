edd6262563008bb46e24631c2b029840257a8833
theDreamBear <512068167@qq.com>
2021-01-06 12:22:48 +0800

    0

--- leetcode/482.密钥格式化.cpp
+++ leetcode/482.密钥格式化.cpp
@@ -3,12 +3,48 @@
  *
  * [482] 密钥格式化
  */
+#include <iostream>
+#include <utility>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
 
 // @lc code=start
 class Solution {
 public:
     string licenseKeyFormatting(string S, int K) {
-
+        string ans;
+        int sz = 0;
+        for (int i = S.size() - 1; i >= 0; --i) {
+            if (S[i] != '-') {
+                char c = S[i];
+                if (islower(c)) {
+                    c = toupper(c);
+                }
+                if (sz < K) {
+                    ans.push_back(c);
+                    ++sz;
+                    if (sz == K) {
+                        ans.push_back('-');
+                        sz = 0;
+                    }
+                }
+            }
+        }
+        if (ans.back() == '-') {
+            ans.pop_back();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 // @lc code=end
