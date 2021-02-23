2cf7e53b82b5526bc8ccb480cdb509c1a52a4f8c
theDreamBear <512068167@qq.com>
2021-02-23 19:25:04 +0800

    0

--- leetcode/491.递增子序列.cpp
+++ leetcode/491.递增子序列.cpp
@@ -23,9 +23,7 @@ using namespace std;
 class Solution {
  public:
     /*
-        去重麻烦, 有问题解法
-
-    */
+     */
     void dfs(set<vector<int>>& ans, vector<int> temp, int pos,
              const vector<int>& nums) {
         // 边界
@@ -34,15 +32,14 @@ class Solution {
                 ans.insert(temp);
             }
             return;
-
-       }
+        }
+        // 不取 pos
         dfs(ans, temp, pos + 1, nums);
-        for (int k = pos; k < nums.size(); ++k) {
-            if (temp.empty() || temp.back() <= nums[k]) {
-                temp.push_back(nums[k]);
-                dfs(ans, temp, k + 1, nums);
-                temp.pop_back();
-            }
+
+        // 取 pos
+        if (temp.empty() || temp.back() <= nums[pos]) {
+            temp.push_back(nums[pos]);
+            dfs(ans, temp, pos + 1, nums);
         }
     }
 
@@ -50,18 +47,19 @@ class Solution {
 
     */
     void dfs_optimize(set<vector<int>>& ans, vector<int> temp, int pos,
-             const vector<int>& nums) {
+                      const vector<int>& nums) {
         // 提前剪枝
-       // if (pos >= nums.size()) {
-            if (temp.size() > 1) {
-                ans.insert(temp);
-            }
-       //     return;
-       //}
-       // dfs(ans, temp, pos + 1, nums);
-       unordered_set<int> visited;
+        // if (pos >= nums.size()) {
+        if (temp.size() > 1) {
+            ans.insert(temp);
+        }
+        //     return;
+        //}
+        // dfs(ans, temp, pos + 1, nums);
+        unordered_set<int> visited;
         for (int k = pos; k < nums.size(); ++k) {
-            if (visited.count(nums[k]) == 0 && (temp.empty() || temp.back() <= nums[k])) {
+            if (visited.count(nums[k]) == 0 &&
+                (temp.empty() || temp.back() <= nums[k])) {
                 visited.insert(nums[k]);
                 temp.push_back(nums[k]);
                 dfs_optimize(ans, temp, k + 1, nums);
@@ -70,10 +68,9 @@ class Solution {
         }
     }
 
-
-    vector<vector<int>> findSubsequences1(vector<int>& nums) {
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
         set<vector<int>> ans;
-        dfs_optimize(ans, {}, 0, nums);
+        dfs(ans, {}, 0, nums);
         return vector<vector<int>>(ans.begin(), ans.end());
     }
 
@@ -81,7 +78,7 @@ class Solution {
         bfs
         0个元素 -> 1 个元素 -> 2 个元素 一层层扩充, 一层层保证最终序列只有一个
     */
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
+    vector<vector<int>> findSubsequences_bfs(vector<int>& nums) {
         vector<vector<int>> ans;
         queue<pair<int, vector<int>>> q;
         q.push({-1, {}});
@@ -94,8 +91,9 @@ class Solution {
                     ans.emplace_back(pr.second);
                 }
                 bitset<300> added;
-                for (int pos = pr.first + 1; pos < nums.size(); ++ pos) {
-                    if (added[nums[pos] + 100] == 0 && (pr.second.empty() || pr.second.back() <= nums[pos])) {
+                for (int pos = pr.first + 1; pos < nums.size(); ++pos) {
+                    if (added[nums[pos] + 100] == 0 &&
+                        (pr.second.empty() || pr.second.back() <= nums[pos])) {
                         added[nums[pos] + 100] = 1;
                         vector<int> temp = pr.second;
                         temp.emplace_back(nums[pos]);
