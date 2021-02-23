8699884cf0f6948879d0d67bea193ad3be55be9f
theDreamBear <512068167@qq.com>
2021-02-23 20:16:50 +0800

    0

--- leetcode/491.递增子序列.cpp
+++ leetcode/491.递增子序列.cpp
@@ -107,6 +107,159 @@ class Solution {
 };
 // @lc code=end
 
+
+/*
+ 官方的 hash 算法
+*/
+class Solution1 {
+public:
+    vector<int> temp;
+    vector<vector<int>> ans;
+    unordered_set<int> s;
+    int n;
+
+    void findSubsequences(int mask, vector<int>& nums) {
+        temp.clear();
+        for (int i = 0; i < n; ++i) {
+            if (mask & 1) {
+                temp.push_back(nums[i]);
+            }
+            mask >>= 1;
+        }
+    }
+
+    bool check() {
+        for (int i = 1; i < temp.size(); ++i) {
+            if (temp[i] < temp[i - 1]) {
+                return false;
+            }
+        }
+        return temp.size() >= 2;
+    }
+
+    int getHash(int base, int mod) {
+        int hashValue = 0;
+        for (const auto &x: temp) {
+            hashValue = 1LL * hashValue * base % mod + (x + 101);
+            hashValue %= mod;
+        }
+        return hashValue;
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        n = nums.size();
+        for (int i = 0; i < (1 << n); ++i) {
+            findSubsequences(i, nums);
+            int hashValue = getHash(263, int(1E9) + 7);
+            if (check() && s.find(hashValue) == s.end()) {
+                ans.push_back(temp);
+                s.insert(hashValue);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+    方法 3 可以多研究研究
+*/
+class Solution3 {
+public:
+    vector<int> temp;
+    vector<vector<int>> ans;
+
+    void dfs(int cur, int last, vector<int>& nums) {
+        if (cur == nums.size()) {
+            if (temp.size() >= 2) {
+                ans.push_back(temp);
+            }
+            return;
+        }
+        if (nums[cur] >= last) {
+            temp.push_back(nums[cur]);
+            dfs(cur + 1, nums[cur], nums);
+            temp.pop_back();
+        }
+        if (nums[cur] != last) {
+            dfs(cur + 1, last, nums);
+        }
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        dfs(0, INT_MIN, nums);
+        return ans;
+    }
+};
+
+
+class Solution4 {
+public:
+   // 判重代码；
+    bool is_first(const vector<int> &num, int last, int pos) {
+        for(int i = last+1; i < pos; i++) {
+            if(num[i] == num[pos]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void dfs(const vector<int> &nums, int last, int pos, vector<int> &stack, vector<vector<int>> &anw) {
+        if(nums.size() == pos) { return; } //到达末尾，直接返回吧
+        // 检查 nums[pos] 是否符合要求
+        if((stack.empty() || nums[pos] >= stack.back()) && is_first(nums, last, pos)) {
+            stack.push_back(nums[pos]);
+            if(stack.size() >= 2) { //大于 2 了，那就放进去吧
+                anw.push_back(stack);
+            }
+            dfs(nums, pos, pos+1, stack, anw); // 继续处理下一个。
+            stack.pop_back(); // 将当前放入这个吐出来。
+        }
+        dfs(nums, last, pos+1, stack, anw);
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int>> anw;
+        vector<int> stack;
+        dfs(nums, -1, 0, stack, anw);
+        return anw;
+    }
+};
+
+
+class Solution5 {
+public:
+    int pre[15];
+    // 判重代码；
+    bool is_first(const vector<int> &num, int last, int pos) {
+        return !(last < pre[pos] && pre[pos] < pos);
+    }
+    void dfs(const vector<int> &nums, int last, int pos, vector<int> &stack, vector<vector<int>> &anw) {
+        if(nums.size() == pos) { return; } //到达末尾，直接范围吧
+        // 检查 nums[pos] 是否符合要求
+        if((stack.empty() || nums[pos] >= stack.back()) && is_first(nums, last, pos)) {
+            stack.push_back(nums[pos]);
+            if(stack.size() >= 2) { //大于 2 了，那就放进去吧
+                anw.push_back(stack);
+            }
+            dfs(nums, pos, pos+1, stack, anw); // 继续处理下一个。
+            stack.pop_back(); // 将当前放入这个吐出来。
+        }
+        dfs(nums, last, pos+1, stack, anw);
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            pre[i] = -1;
+            for(int j = i-1; j >= 0; j--) {
+                if(nums[j] == nums[i]) { pre[i] = j; break; }
+            }
+        }
+        vector<vector<int>> anw;
+        vector<int> stack;
+        dfs(nums, -1, 0, stack, anw);
+        return anw;
+    }
+};
+
+
 int main() {
     vector<int> nums = {4, 6, 7, 7};
     Solution{}.findSubsequences(nums);
