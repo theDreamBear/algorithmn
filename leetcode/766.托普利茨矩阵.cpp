6c16bec756717902560069adb7c902c9e2bb66e9
theDreamBear <512068167@qq.com>
2021-02-26 20:06:29 +0800

    0

--- leetcode/766.托普利茨矩阵.cpp
+++ leetcode/766.托普利茨矩阵.cpp
@@ -21,7 +21,7 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+    bool isToeplitzMatrix1(vector<vector<int>>& matrix) {
         int col = matrix[0].size() - 2;
         int row  = 0;
         while (row < matrix.size() - 1 && col < matrix[0].size() - 1) {
@@ -40,6 +40,18 @@ public:
                 --col;
             } else {
                 ++row;
+            }
+        }
+        return true;
+    }
+
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
             }
         }
         return true;
