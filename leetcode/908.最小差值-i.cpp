95dd6c4359fb33b1ed2eb9d7bf22aeda91c64ad8
theDreamBear <512068167@qq.com>
2021-03-04 21:22:27 +0800

    0

--- leetcode/908.最小差值-i.cpp
+++ leetcode/908.最小差值-i.cpp
@@ -21,9 +21,38 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int smallestRangeI(vector<int>& A, int K) {
+    int smallestRangeI1(vector<int>& A, int K) {
         auto mm = minmax_element(A.begin(), A.end());
         int diff = *mm.second - *mm.first;
+        if (diff <= 2 * K) {
+            return 0;
+        }
+        return diff - 2 * K;
+    }
+
+    pair<int, int> myMinMax(vector<int>& A) {
+        int mm = INT_MAX, ma = INT_MIN, i = 0;
+        if (A.size() % 2 != 0) {
+            ++i;
+            mm = A[0];
+            ma = A[0];
+        }
+        while (i + 1 < A.size()) {
+            auto dd = minmax(A[i], A[i + 1]);
+            if (dd.second > ma) {
+                ma = dd.second;
+            }
+            if (dd.first < mm) {
+                mm = dd.first;
+            }
+            i += 2;
+        }
+        return {mm, ma};
+    }
+
+    int smallestRangeI(vector<int>& A, int K) {
+        auto mm = myMinMax(A);
+        int diff = mm.second - mm.first;
         if (diff <= 2 * K) {
             return 0;
         }
