4caa612a8d54db28968e45b6636c4972c6870d06
theDreamBear <512068167@qq.com>
2021-03-17 02:17:52 +0800

    0

--- leetcode/33.搜索旋转排序数组.cpp
+++ leetcode/33.搜索旋转排序数组.cpp
@@ -35,30 +35,69 @@ public:
 
     /*
         二分
+        写代码前一定要分清楚各种情况
     */
-    int search(vector<int>& nums, int target) {
+    int search2(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
-        if (nums.size() == 2) {
-            if (nums[low] == target) {
-                return low;
-            }
-            if (nums[high] == target) {
-                return high;
-            }
-            return -1;
-        }
         while (low <= high) {
             int mid = (low + high) / 2;
             if (nums[mid] == target) {
                 return mid;
+            }
+            if (nums[low] == target) {
+                return low;
             }   
+            if (nums[high] == target) {
+                return high;
+            }
+
+            // 1. low - high 升序
+            if (nums[low] < target && nums[high] > target) {
+                if (target > nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+                continue;
+            }
+            // 2. low - high  降序
+            if (nums[low] > target && nums[high] < target) {
+                if (target > nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+                continue;
+            }
+            // 3. 左mid
+            if (nums[mid] > nums[low]) {
+                if (target > nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+                continue;
+            }
+            // 4. 右mid
+            if (target > nums[mid] && target < nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
         return -1;
+    }
+
+    /*
+        旋转后两个部分必有一个是有序的
+    */
+    int search(vector<int>& nums, int target) {
+
     }
 };
 // @lc code=end
 
 int main() {
-    vector<int> data = {4, 5, 6, 7, 0, 1 ,2};
-    cout << Solution{}.search(data, 0);
+    vector<int> data = {5, 1, 2, 3, 4};
+    cout << Solution{}.search(data, 1);
 }
