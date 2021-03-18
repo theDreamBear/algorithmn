906c9c76f21b8e1ce85db36d80eb8f3fe606a104
theDreamBear <512068167@qq.com>
2021-03-18 23:47:04 +0800

    0

---
+++ leetcode/45.跳跃游戏-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ *
+ * [45] 跳跃游戏 II
+ */
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <utility>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <ranges.h>
+
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    /*
+        状态定义dp[i] 从0 到 i 最少跳跃次数
+        状态转移方程 
+        dp[i + j] = min(dp[i + j], dp[i] + 1 ) (0 < j <= nums[i])
+        ans : dp.back();
+                  
+    */
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = nums[i]; j > 0; --j) {
+                if ((i + j < nums.size()) && (dp[i + j] > dp[i] + 1)) {
+                    dp[i + j] = dp[i] + 1;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+// @lc code=end
+
+int main() {
+    vector<int> data{2,3,1,1,4};
+    cout << Solution{}.jump(data);
+}
