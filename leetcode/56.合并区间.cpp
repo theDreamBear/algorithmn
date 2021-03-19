e7ee6f980b1cd53c1305d3dadbd55e54ceff948d
theDreamBear <512068167@qq.com>
2021-03-19 20:09:34 +0800

    0

---
+++ leetcode/56.合并区间.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=cpp
+ *
+ * [56] 合并区间
+ */
+#include <iostream>
+#include <utility>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end(), [&](vector<int>& r, vector<int>& l) {
+            if (r[0] < l[0]) {
+                return true;
+            }
+            if (r[0] == l[0] && r[1] < l[1]) {
+                return true;
+            }
+            return false;
+        });
+        int left = -1, right = -1;
+        for(auto& vec : intervals) {
+            if (left == -1) {
+                left = vec[0];
+                right = vec[1];
+            } else {
+                if (right >= vec[0]) {
+                    right = max(right, vec[1]);
+                } else {
+                    ans.push_back({left, right});
+                    left = vec[0];
+                    right = vec[1];
+                }
+            }
+        }
+        if (left != -1) {
+            ans.push_back({left, right});
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
