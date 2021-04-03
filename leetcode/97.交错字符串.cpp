763f1cff2d0d9058c0be2dc659945d239b24c029
theDreamBear <512068167@qq.com>
2021-04-03 21:42:37 +0800

    0

--- leetcode/97.交错字符串.cpp
+++ leetcode/97.交错字符串.cpp
@@ -35,7 +35,7 @@ public:
     vector<vector<int>> dp;
     bool dfs(const string& s1, const string& s2, const string& s3, int i, int j, int k) {
         if (i == s1.size() && j == s2.size()) {
-            dp[i][j] == 1;
+            dp[i][j] = 1;
             return true;
         }
         if (dp[i][j] != INT_MAX) {
@@ -77,7 +77,7 @@ public:
         循环记忆化搜索
         dp[i][j] 前 i, j
     */
-    bool isInterleave2(string s1, string s2, string s3) {
+    bool isInterleave(string s1, string s2, string s3) {
         // 边界
         // 长度必须相等
         if(s1.size() + s2.size() != s3.size()) {
@@ -85,28 +85,15 @@ public:
         }
         vector<vector<bool>> dp2(s1.size() + 1, vector<bool>(s2.size() + 1));
         dp2[0][0] = 1;
-        for (int i = 1; i <= s1.size(); ++i) {
-            if (s1[i - 1] == s3[i - 1]) {
-                dp2[i][0] = true;
-            } else {
-                break;
-            }
-        }
         // 前 i 项
-        for (int i = 1; i <= s2.size(); ++i) {
-            if (s2[i - 1] == s3[i - 1]) {
-                dp2[0][i] = true;
-            } else {
-                break;
-            }
-        }
-        // 前 i 项
-        for (int i = 1; i <= s1.size(); ++i) {
+        for (int i = 0; i <= s1.size(); ++i) {
             // 前 j 项
-            for (int j = 1; j <= s2.size(); ++j) {
-                dp2[i][j] = dp2[i - 1][j] && s1[i - 1] == s3[i + j - 1];
-                if (!dp2[i][j]) {
-                     dp2[i][j] = dp2[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+            for (int j = 0; j <= s2.size(); ++j) {
+                if (i > 0) {
+                    dp2[i][j] = dp2[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                }
+                if (j > 0) {
+                    dp2[i][j] = dp2[i][j] | (dp2[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
                 }
             }
         }
@@ -132,13 +119,36 @@ public:
         }
         vector<bool> dp2(left->size() + 1);
         dp2[0] = true;
+        for (int i = 1; i <= left->size(); ++i) {
+            dp2[i] = dp2[i - 1] && left->at(i - 1) == s3[i - 1];
+        }
+        bool update = true;
         for (int i = 1; i <= right->size(); ++i) {
-            dp2[0] = dp2[0] & ()
+            update = false;
+            dp2[0] = dp2[0] && right->at(i - 1) == s3[i - 1];
+            if (!update) {
+                update = dp2[0];
+            }
             for (int j = 1; j <= left->size(); ++j) {
-
+                bool l = dp2[j - 1] && left->at(j - 1) == s3[i + j - 1];
+                bool r = dp2[j] && right->at(i - 1) == s3[i + j - 1];
+                dp2[j] = l || r;
+                if (!update) {
+                    update = dp2[j];
+                }
+            }
+            if (!update) {
+                break;
             }
         }
+        return dp2.back();
     }
 };
 // @lc code=end
 
+int main() {
+    string s1 = "aabcc";
+    string s2 = "dbbca";
+    string s3 = "aadbbbaccc";
+    cout << Solution{}.isInterleave(s1, s2, s3);
+}
