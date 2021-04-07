11e88ae1f72f127becfca0c91c7863e70a70c162
theDreamBear <512068167@qq.com>
2021-04-07 23:08:52 +0800

    0

--- leetcode/5.最长回文子串.cpp
+++ leetcode/5.最长回文子串.cpp
@@ -54,6 +54,21 @@ public:
         }
         return s.substr(l, len);
     }
+
+    string longestPalindrom1(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), true));
+        int left = 0, sz = 1;
+        for (int j = 0; j < s.size(); ++j) {
+            for (int i = 0; i < j; ++i) {
+                dp[i][j] = ((s[i] == s[j]) & dp[i + 1][j - 1]);
+                if (dp[i][j] && j - i + 1 > sz) {
+                    sz = j - i + 1;
+                    left = i;
+                }
+            }
+        }
+        return s.substr(left, sz);
+    }
 };
 // @lc code=end
 
