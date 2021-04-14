cf975989fe0900bd833566d3c42d435b14ef9dfb
theDreamBear <512068167@qq.com>
2021-04-14 11:43:03 +0800

    0

--- leetcode/201.数字范围按位与.cpp
+++ leetcode/201.数字范围按位与.cpp
@@ -38,14 +38,21 @@ public:
 
     /*
         每一位处理, 对于高位可能会循环好久
+        同样最后一个测试用例超时
     */
-    int rangeBitwiseAnd(int left, int right) {
+    int rangeBitwiseAnd2(int left, int right) {
         int ans = 0x7fffffff;
-        for (int i = 31; i >= 0; --i) {
+        int temp = 0;
+        for (int i = 0; i <= 31; ++i) {
+            if (i > 0) {
+                temp |= (0x1 << (i - 1));
+            }
             // ++j越界的问题
             for (int j = left; j <= right; ++j) {
                 if ((j & (0x1 << i)) == 0) {
                     ans &= ~(0x1 << i);
+                    break;
+                } else if ((left | temp ) >= right) {
                     break;
                 }
                 if (j == right) {
@@ -55,9 +62,32 @@ public:
         }
         return ans;
     }
+
+    /*
+        寻找公共前缀
+    */
+    int rangeBitwiseAnd3(int left, int right) {
+        while (left < right) {
+            right &= (right - 1);
+        }
+        return right;
+    }
+
+    /*
+        逐个消除右边非前缀
+    */
+    int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            ++shift;
+        }
+        return left << shift;
+    }
 };
 // @lc code=end
 
 int main() {
-    cout << Solution{}.rangeBitwiseAnd(2147483646, 0x7fffffff);
+    cout << Solution{}.rangeBitwiseAnd(8, 15);
 }
