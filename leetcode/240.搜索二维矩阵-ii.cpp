5cc442b4166b3287b3dccaa22878b35ea401993e
theDreamBear <512068167@qq.com>
2021-04-18 23:51:22 +0800

    0

---
+++ leetcode/240.搜索二维矩阵-ii.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=cpp
+ *
+ * [240] 搜索二维矩阵 II
+ */
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <utility>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <ranges.h>
+
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                --i;
+            } else {
+                ++j;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
