20cbd44eb42e4b4082daa0da64e930992e27fe5f
nichao <nichao@zuoyebang.com>
2021-07-15 20:15:50 +0800

    0

--- leetcode/863.二叉树中所有距离为-k-的结点.cpp
+++ leetcode/863.二叉树中所有距离为-k-的结点.cpp
@@ -71,7 +71,7 @@ public:
         findPathHelper(node->left, target, track, res);
         track.pop_back();
         if (res == "") {
-        track.push_back('1');
+            track.push_back('1');
             findPathHelper(node->right, target, track, res);
         }
     }
@@ -80,9 +80,9 @@ public:
         string r;
         findPathHelper(root, target, "", r);
         int v = 0;
-        TreeNode* cur = root;
+        TreeNode *cur = root;
         path[cur] = v++;
-        for (auto& c : r) {
+        for (auto &c : r) {
             if (c == '0') {
                 cur = cur->left;
             } else {
@@ -102,7 +102,7 @@ public:
         visit(hash, node->right, v + step, step);
     }
 
-    vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {
+    vector<int> distanceK1(TreeNode *root, TreeNode *target, int k) {
         vector<int> ans;
         unordered_map<TreeNode *, int> path;
         findPath(path, root, target);
@@ -128,6 +128,108 @@ public:
             for (auto &kv1 : hash) {
                 if (abs(kv1.second - v) == k) {
                     ans.push_back(kv1.first->val);
+                }
+            }
+        }
+        return ans;
+    }
+
+    void sub_add(vector<int> &ans, TreeNode *node, int cur, int K) {
+        if (!node) {
+            return;
+        }
+        if (cur == K) {
+            ans.push_back(node->val);
+        }
+        sub_add(ans, node->left, cur + 1, K);
+        sub_add(ans, node->right, cur + 1, K);
+    }
+
+    int dfs(vector<int> &ans, TreeNode *node, TreeNode *target, int K) {
+        if (!node) {
+            return -1;
+        }
+        if (node == target) {
+            sub_add(ans, node, 0, K);
+            return 1;
+        } else {
+            int L = dfs(ans, node->left, target, K);
+            int R = dfs(ans, node->right, target, K);
+            if (L != -1) {
+                if (L == K) {
+                    ans.push_back(node->val);
+
+                } else {
+                    sub_add(ans, node->right, L + 1, K);
+                }
+                return L + 1;
+            } else if (R != -1) {
+                if (R == K) {
+                    ans.push_back(node->val);
+                } else {
+                    sub_add(ans, node->left, R + 1, K);
+                }
+                return R + 1;
+            }
+            return -1;
+        }
+    }
+
+    vector<int> distanceK3(TreeNode *root, TreeNode *target, int k) {
+        vector<int> ans;
+        dfs(ans, root, target, k);
+        return ans;
+    }
+
+    void preOrderTraversal(TreeNode* node, TreeNode* father, unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (!node) {
+            return;
+        }
+        parent[node] = father;
+        preOrderTraversal(node->left, node, parent);
+        preOrderTraversal(node->right, node, parent);
+    }
+
+    vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {
+        vector<int> ans;
+        unordered_map<TreeNode*, TreeNode*> parent;
+        preOrderTraversal(root, NULL, parent);
+        // bfs
+        queue<TreeNode*> q;
+        unordered_set<TreeNode*> seen;
+        q.push(target);
+        seen.insert(target);
+        int dist = -1;
+        while (!q.empty()) {
+            dist++;
+            int sz = q.size();
+            for (int i = 0; i < sz; ++i) {
+                // 左
+                auto node = q.front();
+                q.pop();
+                if (dist == k) {
+                    ans.push_back(node->val);
+                }
+                if (node->left && seen.count(node->left) == 0) {
+                    seen.insert(node->left);
+                    if (dist < k) {
+                        q.push(node->left);
+                    }
+                }
+                // 右
+                if (node->right && seen.count(node->right) == 0) {
+                    seen.insert(node->right);
+                    if (dist < k) {
+                        q.push(node->right);
+                    }
+                }
+                // 父
+                auto father = parent[node];
+                if (father && seen.count(father) == 0) {
+                    seen.insert(father);
+                    if (dist < k) {
+                        q.push(father);
+                    }
                 }
             }
         }
