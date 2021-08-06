62d63380f1b88c5f5ade9ae0b2205eced7e22e86
nichao <nichao@zuoyebang.com>
2021-08-06 16:15:01 +0800

    0

--- leetcode/1659.最大化网格幸福感.cpp
+++ leetcode/1659.最大化网格幸福感.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution1 {
 private:
    int mask_span[729][6];
    int inner_score[729];
@@ -86,6 +86,86 @@ public:
        }
        return dp[last_mask][row][nx][wx] = best;
    }
+
+    int getMaxGridHappiness(int m, int n, int nx, int wx) {
+        this->m = m;
+        this->n = n;
+        this->max_mask = pow(3, n);
+        prepare();
+        return dfs(0, 0, nx, wx);
+    }
+};
+
+class Solution {
+private:
+    // 每一位的值
+    int mask_span[729][6];
+
+    int dp[25][729][7][7];
+
+    // 改变后的值
+    int trans[729][3];
+
+    int max_mask;
+    int m, n;
+
+    void prepare() {
+        int ex = max_mask / 3;
+        for (int mask = 0; mask < max_mask; mask++) {
+            for (int mask_temp = mask, j = n - 1; j >= 0; j--) {
+                mask_span[mask][j] = mask_temp % 3;
+                mask_temp /= 3;
+            }
+            int mask_new = mask % ex;
+            trans[mask][0] = mask_new * 3;
+            trans[mask][1] = mask_new * 3 + 1;
+            trans[mask][2] = mask_new * 3 + 2;
+        }
+        memset(dp, -1, sizeof dp);
+    }
+
+    int cal(int x, int y) {
+        if (x * y == 0) {
+            return 0;
+        }
+        if (x * y == 1) {
+            return -60;
+        }
+        if (x * y == 4) {
+            return 40;
+        }
+        return -10;
+    }
+
+    int dfs(int pos, int borderline, int nx, int wx) {
+        if (pos == m * n || nx + wx == 0) {
+            return 0;
+        }
+        if (dp[pos][borderline][nx][wx] != -1) {
+            return dp[pos][borderline][nx][wx];
+        }
+        int x = pos / n;
+        int y = pos % n;
+        // 不选择
+        int best = dfs(pos + 1, trans[borderline][0], nx, wx);
+        // 内向
+        if (nx > 0) {
+            int next = dfs(pos + 1, trans[borderline][1], nx - 1, wx);
+            int top = (x == 0) ? 0 : cal(1, mask_span[borderline][0]);
+            int left = (y == 0)? 0 : cal(1, mask_span[borderline][n - 1]);
+            best = max(best, 120 + next + top + left);
+        }
+        if (wx > 0) {
+            int next = dfs(pos + 1, trans[borderline][2], nx, wx - 1);
+            int top = (x == 0) ? 0 : cal(2, mask_span[borderline][0]);
+            int left = (y == 0)? 0 : cal(2, mask_span[borderline][n - 1]);
+            best = max(best, 40 + next + top + left);
+        }
+        return dp[pos][borderline][nx][wx] = best;
+    }
+
+
+public:
 
     int getMaxGridHappiness(int m, int n, int nx, int wx) {
         this->m = m;
