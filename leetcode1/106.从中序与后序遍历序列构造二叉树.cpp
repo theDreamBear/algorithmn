b660f4d588452e33fc4bb1db7482882919ac822b
nichao <nichao@zuoyebang.cc>
2022-07-29 20:25:52 +0800

    0

--- leetcode1/106.从中序与后序遍历序列构造二叉树.cpp
+++ leetcode1/106.从中序与后序遍历序列构造二叉树.cpp
@@ -36,8 +36,57 @@ public:
         return root;
     }
 
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    TreeNode* buildTree_recursion(vector<int>& inorder, vector<int>& postorder) {
         return buildTreeHelper(inorder, 0, (int)inorder.size() - 1, postorder, 0, (int)postorder.size() - 1);
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.empty()) {
+            return nullptr;
+        }
+        struct Item {
+            int l1, h1, l2, h2;
+            TreeNode* TreeNode::* child;
+            TreeNode* father;
+            Item() = default;
+            Item(int l1, int h1, int l2, int h2, TreeNode* TreeNode::* child, TreeNode* fa) : l1(l1), h1(h1), l2(l2), h2(h2), child(child), father(fa){}
+        };
+        TreeNode* root = nullptr;
+        stack<Item> st;
+        st.push(Item(0, (int)inorder.size() - 1, 0, (int)inorder.size() - 1, nullptr, nullptr));
+        while (!st.empty()) {
+            auto item = st.top();
+            st.pop();
+            int l1 = item.l1;
+            int h1 = item.h1;
+            int l2 = item.l2;
+            int h2 = item.h2;
+            auto child = item.child;
+            auto father = item.father;
+            if (l1 > h1) {
+                continue;
+            }
+            int val = postorder[h2];
+            TreeNode* node = new TreeNode(val);
+            if (nullptr == father) {
+                root = node;
+            } else {
+                father->*child = node;
+            }
+            if (l1 == h1) {
+                continue;
+            }
+            int i = l1;
+            for (; i <= h1; i++) {
+                if (inorder[i] == val) {
+                    break;
+                }
+            }
+            int lsz = i - l1;
+            st.push(Item(l1, i - 1, l2, l2 + lsz - 1, &TreeNode::left, node));
+            st.push(Item{i + 1, h1, l2 + lsz, h2 - 1, &TreeNode::right, node});
+        }
+        return root;
     }
 };
 // @lc code=end
