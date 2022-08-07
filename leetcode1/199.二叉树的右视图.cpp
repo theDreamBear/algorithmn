7bb14bb0ae6d8c79c8e484aaedc0f1e340ca9260
nichao <nichao@zuoyebang.cc>
2022-08-07 16:26:44 +0800

    0

---
+++ leetcode1/199.二叉树的右视图.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=cpp
+ *
+ * [199] 二叉树的右视图
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                auto node = q.front();
+                q.pop();
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+                if (i == sz - 1) {
+                    ans.push_back(node->val);
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
