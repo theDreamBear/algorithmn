eb0e45ece66ba8491ee89749e04d5ad28bbacfdb
nichao <nichao@zuoyebang.com>
2022-08-10 02:08:12 +0800

    0

---
+++ leetcode1/46.全排列.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=cpp
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if (nums.empty()) {
+            return {};
+        }
+        vector<vector<int>> ans;
+        vector<int> temp = nums;
+        function<void(int)> backtrack = [&](int pos) {
+            if (pos >= nums.size()) {
+                ans.push_back(temp);
+                return;
+            }
+            for (int i = pos; i < nums.size(); i++) {
+                swap(temp[i], temp[pos]);
+                backtrack(pos + 1);
+                swap(temp[i], temp[pos]);
+                //backtrack(i + 1);
+            }
+        };
+        backtrack(0);
+        return ans;
+    }
+};
+
+// @lc code=end
+
