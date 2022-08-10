18c00ee06aeba0afd4d8a747d3613aed75bc39d1
nichao <nichao@zuoyebang.cc>
2022-08-10 12:49:04 +0800

    0

--- leetcode1/39.组合总和.cpp
+++ leetcode1/39.组合总和.cpp
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum1(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> temp;
@@ -31,6 +31,48 @@ public:
         backtrack(0, 0);
         return ans;
     }
+
+    struct State {
+        vector<int> vec;
+        int next_pos{};
+        int cur_sum{};
+
+        State() = default;
+
+        State(State &state, int pos, int val) {
+            vec = state.vec;
+            next_pos = pos;
+            cur_sum = state.cur_sum;
+
+            vec.push_back(val);
+            cur_sum += val;
+        }
+    };
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        auto state = new State;
+        sort(candidates.begin(), candidates.end());
+        stack<State *> st;
+        st.push(state);
+        while (!st.empty()) {
+            auto s = st.top();
+            st.pop();
+            if (s->cur_sum > target) {
+                continue;
+            }
+            if (s->cur_sum == target) {
+                ans.push_back(s->vec);
+                continue;
+            }
+            for (int i = s->next_pos; i < candidates.size(); i++) {
+                State* next_state = new State(*s, i, candidates[i]);
+                st.push(next_state);
+            }
+        }
+        return ans;
+    }
+
 };
 // @lc code=end
 
