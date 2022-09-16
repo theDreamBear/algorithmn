c5b16046bb5246a310f9c70f4e5dfd30012d239b
nichao <nichao@zuoyebang.cc>
2022-09-16 18:17:05 +0800

    0

--- leetcode1/1237.找出给定方程的正整数解.cpp
+++ leetcode1/1237.找出给定方程的正整数解.cpp
@@ -75,7 +75,7 @@ public:
         return ans;
     }
 
-    vector<vector<int>> findSolution(CustomFunction &customfunction, int z) {
+    vector<vector<int>> findSolution3(CustomFunction &customfunction, int z) {
         vector<vector<int>> ans;
         int x = 1, y = 1000;
         while (x <= 1000 && y >= 1) {
@@ -92,6 +92,219 @@ public:
         }
         return ans;
     }
+
+    enum Direction {
+        UP = 0, RIGHT = 2
+    };
+
+    class MoveForward {
+    public:
+        Direction d = UP;
+
+        Direction getDirection() const {
+            return d;
+        }
+
+        void ChangeDirection() {
+            if (d == UP) {
+                d = RIGHT;
+            } else {
+                d = UP;
+            }
+        }
+    };
+
+    class State {
+    public:
+        State() {
+            x = 1;
+            y = 1000;
+        }
+
+        int x, y;
+        MoveForward d;
+    };
+
+    static bool ok(State &s) {
+        return s.x <= 1000 && s.y >= 1;
+    }
+
+
+    class NormalStrategy {
+    public:
+        virtual void changeState(State &next) {
+            if (next.d.getDirection() == UP) {
+                next.y--;
+            } else {
+                next.x++;
+            }
+        }
+
+        bool canFit(CustomFunction &customfunction, State &s, int z) {
+            // cout << s.x << "\t" << s.y << "\t";
+            State next = s;
+            changeState(next);
+            // cout << next.x << "\t" << next.y << endl;
+            // 是否是正常状态
+            if (!ok(next)) {
+                return false;
+            }
+            int v = customfunction.f(next.x, next.y);
+            do {
+                if (v == z) {
+                    break;
+                }
+                if (next.d.getDirection() == UP) {
+                    if (v < z) {
+                        return false;
+                    }
+                    break;
+                }
+                if (next.d.getDirection() == RIGHT) {
+                    if (v > z) {
+                        return false;
+                    }
+                }
+            } while (false);
+            s = next;
+            return true;
+        }
+    };
+
+    class HalfStrategy : public NormalStrategy {
+    public:
+        void changeState(State &next) override {
+            if (next.d.getDirection() == UP) {
+                next.y /= 2;
+                return;
+            } else {
+                next.x = (next.x + 1000) / 2;
+            }
+        }
+    };
+
+    class HalfHalfStrategy : public NormalStrategy {
+    public:
+        void changeState(State &next) override {
+            if (next.d.getDirection() == UP) {
+                next.y = next.y * 3 / 4;
+                return;
+            } else {
+                next.x = (3000 + next.x) / 4;
+            }
+        }
+    };
+
+    class TenStrategy : public NormalStrategy {
+    public:
+        void changeState(State &next) override {
+            if (next.d.getDirection() == UP) {
+                next.y -= 10;
+                return;
+            } else {
+                next.x += 10;
+            }
+        }
+    };
+
+    class StateStrategy {
+    public:
+        int up_index = 0;
+        int right_index = 0;
+        NormalStrategy *strategy[4] = {new HalfStrategy, new HalfHalfStrategy, new TenStrategy, new NormalStrategy};
+
+        void try_most(CustomFunction &customfunctionm, State &s, int z, int &index) {
+            // 尝试尽最大可能替换
+            while (index <= 2) {
+                if (strategy[index]->canFit(customfunctionm, s, z)) {
+                    return;
+                }
+                index++;
+            }
+            // 委屈求全替换
+            strategy[index]->changeState(s);
+        }
+
+        void try_most_right(CustomFunction &customfunctionm, State &s, int z) {
+            try_most(customfunctionm, s, z, right_index);
+        }
+
+        void try_most_up(CustomFunction &customfunctionm, State &s, int z) {
+            try_most(customfunctionm, s, z, up_index);
+        }
+    };
+
+    class Saver {
+    public:
+        vector<vector<int>> data;
+
+        virtual void push(int x, int y) {
+            data.push_back({x, y});
+        }
+    };
+
+    class StateMachine {
+    public:
+        State &s;
+        CustomFunction &customfunction;
+        void *data;
+        StateStrategy ss;
+
+        StateMachine(CustomFunction &customfunction, State &s, void *data) : customfunction(customfunction), s(s),
+                                                                             data(data) {}
+
+        void equal_next() {
+            s.x++;
+            s.y--;
+        }
+
+        void equal_state() {
+            ((Saver *) (data))->push(s.x, s.y);
+            equal_next();
+        }
+
+        void try_most_right(int z) {
+            ss.try_most_right(customfunction, s, z);
+        }
+
+        void try_most_up(int z) {
+            ss.try_most_up(customfunction, s, z);
+        }
+
+        void nextState(int z) {
+            int v = customfunction.f(s.x, s.y);
+            if (v == z) {
+                equal_state();
+                return;
+            }
+            bool one = v > z;
+            auto d = s.d.getDirection();
+            // 是否需要调整运动方向
+            if (one + d == 0 || one + d == 3) {
+                s.d.ChangeDirection();
+            }
+            if (s.d.getDirection() == UP) {
+                try_most_up(z);
+            } else {
+                try_most_right(z);
+            }
+        }
+
+        void run(int z) {
+            while (ok(s)) {
+                nextState(z);
+            }
+        }
+    };
+
+    vector<vector<int>> findSolution(CustomFunction &customfunction, int z) {
+        State s;
+        Saver sa;
+        StateMachine machine(customfunction, s, &sa);
+        machine.run(z);
+        return sa.data;
+    }
 };
+
 // @lc code=end
 
