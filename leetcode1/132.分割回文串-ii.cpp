0dffc460ecb2a0ac3ed84ecff43f03a49a7b365c
nichao <nichao@zuoyebang.cc>
2022-10-08 20:08:21 +0800

    0

--- leetcode1/132.分割回文串-ii.cpp
+++ leetcode1/132.分割回文串-ii.cpp
@@ -20,10 +20,10 @@ public:
 
     // 动态规划
     // 状态 前i个字符的最小切分次数
-    // 转移方程  dp[i] = min(dp[j - 1] + 1) 且 s[j, i]为回文串
+    // 转移方程  dp[i] = min(dp[j - 1] + 1) 且 s[j - 1, i - 1]为回文串
     // dp[0] = 0
     // 答案 dp[n]
-    int minCut(string s) {
+    int minCut2(string s) {
         if (s.size() == 1) {
             return 0;
         }
@@ -41,6 +41,58 @@ public:
             }
             for (int j = 2; j < i; j++) {
                 if (isPara(s.c_str(), j - 1, i - 1)) {
+                    dp[i] = min(dp[i], dp[j - 1] + 1);
+                }
+            }
+        }
+        return dp.back();
+    }
+
+    void preProcess(const string& s, vector<vector<bool>> & pp) {
+        for (int i = 0; i < s.size(); i++) {
+            pp[i][i] = true;
+            for (int l = i - 1, r = i + 1; l >= 0 && r < s.size(); l--, r++) {
+                if (s[l] == s[r]) {
+                    pp[l][r] = pp[l + 1][r - 1];
+                }
+            }
+            int j = i + 1;
+            if (j >= s.size()) {
+                continue;
+            }
+            if (s[i] == s[j]) {
+                pp[i][j] = true;
+            }
+            for (int l = i - 1, r = j + 1; l >= 0 && r < s.size(); l--, r++) {
+                if (s[l] == s[r]) {
+                    pp[l][r] = pp[l + 1][r - 1];
+                }
+            }
+        }
+    }
+
+    // s[j - 1, i - 1]为回文串 是否为回文串这个可以预处理求的
+    int minCut(string s) {
+        if (s.size() == 1) {
+            return 0;
+        }
+        int n = s.size();
+        vector<vector<bool>> pp(n, vector<bool>(n));
+        preProcess(s, pp);
+
+        vector<int> dp(n + 1);
+        // 前0个字符
+        dp[0] = 0;
+        // 前i 个字符的情况
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + 1;
+            // 整体是个回文串
+            if (pp[0][i - 1]) {
+                dp[i] = 0;
+                continue;
+            }
+            for (int j = 2; j < i; j++) {
+                if (pp[j - 1][i - 1]) {
                     dp[i] = min(dp[i], dp[j - 1] + 1);
                 }
             }
