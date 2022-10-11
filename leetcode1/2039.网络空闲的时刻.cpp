bfacc7334c9801da5ba81330bdc49b53279912e6
nichao <nichao@zuoyebang.cc>
2022-10-11 19:00:22 +0800

    0

--- leetcode1/2039.网络空闲的时刻.cpp
+++ leetcode1/2039.网络空闲的时刻.cpp
@@ -54,7 +54,7 @@ public:
         return ans;
     }
 
-    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+    int networkBecomesIdle2(vector<vector<int>>& edges, vector<int>& patience) {
         int n = (int)patience.size();
         vector<Vertex> G(n);
         for (auto& vec: edges) {
@@ -75,6 +75,42 @@ public:
                 if (distacne[adj] > distacne[node] + 1) {
                     distacne[adj] = distacne[node] + 1;
                     q.push(adj);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            int rtt = distacne[i] * 2;
+            int left = (rtt % patience[i] == 0) ? patience[i] : rtt % patience[i];
+            ans = max(rtt * 2 - left + 1, ans);
+        }
+        return ans;
+    }
+
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        int n = (int)patience.size();
+        vector<Vertex> G(n);
+        for (auto& vec: edges) {
+            int from = vec[0];
+            int to = vec[1];
+            G[from].adjiacent.push_back(to);
+            G[to].adjiacent.push_back(from);
+        }
+        vector<int> distacne(n, INT_MAX);
+        vector<bool> visited(n);
+        distacne[0] = 0;
+        queue<int> q;
+        q.push(0);
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                int node = q.front();
+                q.pop();
+                for (auto adj: G[node].adjiacent) {
+                    if (distacne[adj] == INT_MAX) {
+                        distacne[adj] = distacne[node] + 1;
+                        q.push(adj);
+                    }
                 }
             }
         }
