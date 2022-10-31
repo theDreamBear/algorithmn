b925a44c7505c798d374e4b1adb6c393a89768ea
nichao <nichao@zuoyebang.cc>
2022-10-31 17:43:29 +0800

    0

---
+++ python/222.完全二叉树的节点个数.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=222 lang=python3
+#
+# [222] 完全二叉树的节点个数
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def treeHigh(root):
+    h = 0
+    while root:
+        h += 1
+        root = root.left
+    return h
+
+
+def exist(root, value, index):
+    while root and index > 1:
+        v = (1 << (index - 2))
+        if value & v:
+            value ^= v
+            root = root.right
+        else:
+            root = root.left
+        index -= 1
+    return root is not None
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        # 空
+        if root is None:
+            return 0
+        high = treeHigh(root)
+        left = 1 << (high - 1)
+        right = (1 << high) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if exist(root, mid, high):
+                left = mid
+            else:
+                right = mid
+        if exist(root, right, high):
+            return right
+        return left
+# @lc code=end
+
