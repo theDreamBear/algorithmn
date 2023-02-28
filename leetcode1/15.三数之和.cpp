7d189a5ba7e697162b91b39db6fdec19b7703ccb
theDreamBear <512068167@qq.com>
2023-03-01 01:10:14 +0800

    0

--- leetcode1/15.三数之和.cpp
+++ leetcode1/15.三数之和.cpp
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum_bad(vector<int>& nums) {
         // 最直观的思路就是， 三层循环
         // 3e3 * 3e3 * 3e3 = 2.7e10 超时
 
@@ -23,12 +23,14 @@ public:
             }
         }
         set<vector<int>> ans;
+        // 这个地方的时间复杂度有问题
+        // 
         for (int i = 0; i < nums.size(); i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             if (mp.count(-nums[i])) {
-                for (auto& p : mp[-nums[i]]) {
+                for (auto p : mp[-nums[i]]) {
                     if (p.first > i && p.second > i) {
                         vector<int> temp{nums[i], nums[p.first], nums[p.second]};
                         sort(temp.begin(), temp.end());
@@ -39,7 +41,27 @@ public:
         }
         return vector<vector<int>>(ans.begin(), ans.end());
     }
-};
 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int, int> mostRight;
+        set<vector<int>> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            mostRight[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.size(); j++) {
+                int target = 0 - nums[i] - nums[j];
+                if (mostRight.count(target) && mostRight[target] > j) {
+                    ans.insert({nums[i], nums[j], target});
+                }
+            }
+        }
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
 // @lc code=end
 
