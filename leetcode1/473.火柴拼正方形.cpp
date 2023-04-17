6890934411133d679e7069c75909eb086710ae8e
nichao <nichao@zuoyebang.com>
2023-04-17 17:50:14 +0800

    0

--- leetcode1/473.火柴拼正方形.cpp
+++ leetcode1/473.火柴拼正方形.cpp
@@ -81,7 +81,7 @@ public:
     }
 };
 
-class Solution {
+class Solution3 {
 public:
     bool dfs(int index, vector<int> &matchsticks, vector<int> &sums, int target) {
         if (index >= matchsticks.size()) {
@@ -118,9 +118,36 @@ public:
         if (sum % 4 != 0) {
             return false;
         }
+        sort(matchsticks.rbegin(), matchsticks.rend());
         int target = sum / 4;
         vector<int> sums(4);
         return dfs(0, matchsticks, sums, target);
+    }
+};
+
+class Solution {
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        int totalLen = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        if (totalLen % 4 != 0) {
+            return false;
+        }
+        int len = totalLen / 4, n = matchsticks.size();
+        vector<int> dp(1 << n, -1);
+        dp[0] = 0;
+        for (int s = 1; s < (1 << n); s++) {
+            for (int k = 0; k < n; k++) {
+                if ((s & (1 << k)) == 0) {
+                    continue;
+                }
+                int s1 = s & ~(1 << k);
+                if (dp[s1] >= 0 && dp[s1] + matchsticks[k] <= len) {
+                    dp[s] = (dp[s1] + matchsticks[k]) % len;
+                    break;
+                }
+            }
+        }
+        return dp[(1 << n) - 1] == 0;
     }
 };
 // @lc code=end
