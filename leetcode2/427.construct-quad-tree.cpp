f946326cfc06c7b7ca46ab67e9fb6ed85e126b3e
nichao <nichao@zuoyebang.com>
2024-01-19 20:36:29 +0800

    0

--- leetcode2/427.construct-quad-tree.cpp
+++ leetcode2/427.construct-quad-tree.cpp
@@ -23,6 +23,42 @@ using namespace std;
 #include <unordered_set>
 #include <utility>
 #include <vector>
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
 // @lcpr-template-end
 // @lc code=start
 /*
@@ -65,12 +101,246 @@ public:
 };
 */
 
-class Solution {
-public:
-    Node* construct(vector<vector<int>>& grid) {
+const int dx[4] = {0, 0, 1, 1};
+const int dy[4] = {0, 1, 1, 0};
 
+Node* Node:: *children[4] = {&Node::topLeft, &Node::topRight, &Node::bottomRight, &Node::bottomLeft};
+
+class Solution1 {
+public:
+    struct One {
+        int x, y, rows, cols;
+
+        One(){
+            x = y = rows = cols = 0;
+        }
+
+        void setBase(int nx, int ny) {
+            x = nx;
+            y = ny;
+        }
+
+        void setRC(int r, int c) {
+            rows = r;
+            cols = c;
+        }
+    };
+
+    bool allSame(const vector<vector<int>>& grid, const One& one) {
+        bool value = grid[one.x][one.y];
+        for (int i = 0; i < one.rows; i++) {
+            for (int j = 0; j < one.cols; j++) {
+                if (grid[one.x + i][one.y + j] != value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    Node* construct(const vector<vector<int>>& grid, const One& one) {
+        // 边界条件
+        if (one.rows <= 0 || one.cols <= 0 || one.rows != one.cols) {
+            assert("rc invalid");
+        }
+        // 只剩一个
+        if (one.rows == 1 && one.cols == 1 ) {
+            Node* node = new Node(grid[one.x][one.y], true);
+            return node;
+        }
+        // 全相同
+        if (allSame(grid, one)) {
+            Node* node = new Node(grid[one.x][one.y], true);
+            return node;
+        }
+        // 切分
+        Node* root = new Node(false, false);
+        int half = one.rows >> 1;
+        for (int d = 0; d < 4; d++) {
+            int nx = one.x + dx[d] * half;
+            int ny = one.y + dy[d] * half;
+            One child;
+            child.setBase(nx, ny);
+            child.setRC(half, half);
+            root->*children[d] = construct(grid, child);
+        }
+        return root;
+    }
+
+
+    Node* construct(vector<vector<int>>& grid) {
+        One one;
+        one.setBase(0, 0);
+        one.setRC(grid.size(), grid[0].size());
+        return construct(grid, one);
     }
 };
+
+
+class Solution {
+public:
+    struct One {
+        int x, y, rows, cols;
+
+        One(){
+            x = y = rows = cols = 0;
+        }
+
+        void setBase(int nx, int ny) {
+            x = nx;
+            y = ny;
+        }
+
+        void setRC(int r, int c) {
+            rows = r;
+            cols = c;
+        }
+    };
+
+    bool allSame(const vector<vector<int>>& grid, const One& one) {
+        bool value = grid[one.x][one.y];
+        for (int i = 0; i < one.rows; i++) {
+            for (int j = 0; j < one.cols; j++) {
+                if (grid[one.x + i][one.y + j] != value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    class IteItem {
+    private:
+        void _get_one_item() {
+            d++;
+            if (d >= 4) {
+                this->ptr = nullptr;
+                return;
+            }
+            int nx = x + dx[d] * half;
+            int ny = y + dy[d] * half;
+            one.setBase(nx, ny);
+            one.setRC(half, half);
+            this->ptr = children[d];
+            return;
+        }
+
+        IteItem() {
+            half = 0;
+            ptr = nullptr;
+        }
+    public:
+        int x, y;
+        One one;
+        Node* Node:: *ptr;
+        int d, half;
+
+        IteItem(const One one, int half):ptr(nullptr){
+            x = one.x;
+            y = one.y;
+            this->one = one;
+            d = -1;
+            this->half = half;
+            if (half > 0) {
+                _get_one_item();
+            }
+        }
+
+        static IteItem end() {
+            return {};
+        }
+
+        bool operator!= (const IteItem& rhs) const{
+            return ptr != rhs.ptr;
+        }
+
+        IteItem& operator++() {
+            _get_one_item();
+            return *this;
+        }
+    };
+
+    class Ite {
+    private:
+        const One one;
+        int half;
+    public:
+        Ite(const One& one, int half):one(one) {
+            this->half = half;
+        }
+
+        IteItem begin() {
+            return IteItem(one, half);
+        }
+
+        IteItem end() {
+            return IteItem::end();
+        }
+
+    };
+
+
+    Node* construct(const vector<vector<int>>& grid, const One& one) {
+        // 边界条件
+        if (one.rows <= 0 || one.cols <= 0 || one.rows != one.cols) {
+            assert("rc invalid");
+        }
+        // 只剩一个
+        if (one.rows == 1 && one.cols == 1 ) {
+            Node* node = new Node(grid[one.x][one.y], true);
+            return node;
+        }
+        // 全相同
+        if (allSame(grid, one)) {
+            Node* node = new Node(grid[one.x][one.y], true);
+            return node;
+        }
+        // 切分
+        Node* root = new Node(false, false);
+        Ite ite(one, one.rows >> 1);
+        for (auto beg = ite.begin(); beg != ite.end(); ++beg) {
+            root->*beg.ptr = construct(grid, beg.one);
+        }
+        return root;
+    }
+
+
+    Node* construct(vector<vector<int>>& grid) {
+        One one;
+        one.setBase(0, 0);
+        one.setRC(grid.size(), grid[0].size());
+        return construct(grid, one);
+    }
+};
+
+// 这个解乏的好处就是， 代码看起来简洁
+class Solution2 {
+public:
+    Node* construct(vector<vector<int>>& grid, int r0, int c0, int r1, int c1) {
+        int nx = (r0 + r1) >> 1;
+        int ny = (c0 + c1) >> 1;
+        for (int i = r0; i < r1; i++) {
+            for (int j = c0; j < c1; j++) {
+                if (grid[i][j] != grid[r0][c0]) {
+                    return new Node {
+                        0,
+                        false,
+                        construct(grid, r0, c0, nx, ny),
+                        construct(grid, r0, ny, nx, c1),
+                        construct(grid, nx, c0, r1, ny),
+                        construct(grid, nx, ny, r1, c1)
+                    };
+                }
+            }
+        }
+        return new Node(grid[r0][c0], true);
+    }
+
+    Node* construct(vector<vector<int>>& grid) {
+        return construct(grid, 0, 0, grid.size(), grid.size());
+    }
+};
+
 // @lc code=end
 
 
