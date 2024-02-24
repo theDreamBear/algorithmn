d506dca728c12d77f99c65424affd4f9b741d01a
nichao <nichao@zuoyebang.cc>
2024-02-24 14:57:42 +0800

    0

---
+++ leetcode2/LCR 155.将二叉搜索树转化为排序的双向链表.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=LCR 155 lang=cpp
+ * @lcpr version=30117
+ *
+ * [LCR 155] 将二叉搜索树转化为排序的双向链表
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+// @lcpr-template-end
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+class Solution {
+public:
+    Node* treeToDoublyList(Node* root) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,2,5,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
