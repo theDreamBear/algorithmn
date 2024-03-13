65c1b15b27db3427cd018bd5906e9c96dcf931b2
nichao <nichao@zuoyebang.com>
2024-03-13 15:28:12 +0800

    0

--- leetcode2/LCR 028.扁平化多级双向链表.cpp
+++ leetcode2/LCR 028.扁平化多级双向链表.cpp
@@ -46,12 +46,17 @@ public:
 
 class Solution {
 public:
-    Node* flatten(Node* head) {
+    /*
+        节点不是真正的双向链表
+        头节点的 pre 为空
+        尾节点的 next也为空
+    */
+    Node* flatten_bad(Node* head) {
         if (!head) {
             return head;
         }
         Node* next = head->next;
-        if (next and next != head) {
+        if (next != head) {
             // 保证next 是一个双向链表
             next->prev = head->prev;
             head->prev->next = next;
@@ -69,7 +74,7 @@ public:
             nt->next = head;
         }
 
-        if (next and next != head) {
+        if (next != head) {
             auto fc = flatten(next);
             Node* nt = fc->prev;
 
@@ -79,6 +84,72 @@ public:
             nt->next = head;
             head->prev = nt;
         }
+        return head;
+    }
+
+    Node* flatten_1(Node* head) {
+        if (!head) {
+            return head;
+        }
+        Node* cur = head;
+        while (cur) {
+            if (cur->child) {
+                Node* next = cur->next;
+
+                Node* p = cur->child;
+                Node* pt = p;
+                while (pt->next) {
+                    pt = pt->next;
+                }
+
+                cur->next = p;
+                p->prev = cur;
+
+                if (next) {
+                    pt->next = next;
+                    next->prev = pt;
+                }
+                // 需要重置child, 这一步容易遗漏
+                cur->child = nullptr;
+            }
+            cur = cur->next;
+        };
+        return head;
+    }
+
+    pair<Node*, Node*> flattenHelper(Node* head) {
+        if (!head) {
+            return {head, head};
+        }
+        Node* cur = head;
+        while (cur && !cur->child) {
+            if (cur->next) {
+                cur = cur->next;
+            } else {
+                break;
+            }
+        }
+        if (!cur->child) {
+            return {head, cur};
+        }
+        auto [ch, ct] = flattenHelper(cur->child);
+        auto [nh, nt] = flattenHelper(cur->next);
+        cur->child = NULL;
+        // 后序遍历
+        cur->next = ch;
+        ch->prev = cur;
+        if (nh) {
+            ct->next = nh;
+            nh->prev = ct;
+        }
+        return {head, nh ? nt : ct};
+    }
+
+    Node* flatten(Node* head) {
+        if (!head) {
+            return head;
+        }
+        flattenHelper(head);
         return head;
     }
 };
