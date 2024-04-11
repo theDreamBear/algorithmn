ba33dec74aad70988d10ba027383aba34d26e65c
nichao <nichao@zuoyebang.com>
2024-04-11 19:32:06 +0800

    0

--- leetcode2/1766.tree-of-coprimes.cpp
+++ leetcode2/1766.tree-of-coprimes.cpp
@@ -77,19 +77,17 @@ public:
     }
 };
 
-class Solution {
+class Solution2 {
 public:
     class graph {
      public:
         int n;
         vector<pair<int, vector<int>>> g;
-        vector<int> marked;
         // 每一个值出现的层级
-        vector<vector<int>> pre;
-        vector<vector<int>> pri;
+        vector<vector<pair<int, int>>> pre;
         vector<vector<int>> hz;
 
-        graph(vector<int>& nums, vector<vector<int>>& edges) : n(nums.size()), g(n), marked(n), pre(51), pri(51), hz(51){
+        graph(vector<int>& nums, vector<vector<int>>& edges) : n(nums.size()), g(n), pre(51), hz(51){
             for (int i = 0; i < n; i++) {
                 g[i] = {nums[i], {}};
             }
@@ -99,6 +97,15 @@ public:
                 g[to].second.push_back(from);
             }
             for (int i = 1; i <= 50; i++) {
+                for (int j = 1; j <= 50; j++) {
+                    if (gcd(i, j) == 1) {
+                        hz[i].push_back(j);
+                    }
+                }
+            }
+            /* 错误代码
+            错误原因 gcd(1, 1) == 1 漏掉了(1, 1)
+            for (int i = 1; i <= 50; i++) {
                 for (int j = i + 1; j <= 50; j++) {
                     if (gcd(i, j) == 1) {
                         hz[i].push_back(j);
@@ -106,6 +113,7 @@ public:
                     }
                 }
             }
+            */
         }
 
         int getAns(int val) {
@@ -113,32 +121,164 @@ public:
             int ans = -1;
             for (auto v : hz[val]) {
                 if (!pre[v].empty()) {
-                    if (pre[v].back() > r) {
-                        ans = pri[v].back();
-                        r = pre[v].back();
+                    if (pre[v].back().first > r) {
+                        ans = pre[v].back().second;
+                        r = pre[v].back().first;
                     }
                 }
             }
             return ans;
         }
 
+        void dfs(vector<int>& ans, int i, int fa, int rank) {
+            int val = g[i].first;
+            ans[i] = getAns(val);
+            pre[val].push_back({rank, i});
+            for (auto& adj: g[i].second) {
+                if (adj != fa) {
+                    dfs(ans, adj, i, rank + 1);
+                }
+            }
+            pre[val].pop_back();
+        }
+
+        void solve(vector<int>& ans) {
+            dfs(ans, 0, -1, 0);
+        }
+    };
+    vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& edges) {
+        vector<int> ans(nums.size(), -1);
+        graph g(nums, edges);
+        g.solve(ans);
+        return ans;
+    }
+};
+
+class Solution3 {
+public:
+    class graph {
+     public:
+        int n;
+        vector<pair<int, vector<int>>> g;
+        vector<int> marked;
+        // 每一个值出现的层级
+        vector<vector<int>> pre;
+        vector<vector<int>> hz;
+
+        graph(vector<int>& nums, vector<vector<int>>& edges) : n(nums.size()), g(n), marked(n), pre(51), hz(51){
+            for (int i = 0; i < n; i++) {
+                g[i] = {nums[i], {}};
+            }
+            for (auto& vec : edges) {
+                int from = vec[0], to = vec[1];
+                g[from].second.push_back(to);
+                g[to].second.push_back(from);
+            }
+            for (int i = 1; i <= 50; i++) {
+                for (int j = 1; j <= 50; j++) {
+                    if (gcd(i, j) == 1) {
+                        hz[i].push_back(j);
+                    }
+                }
+            }
+            /* 错误代码
+            错误原因 gcd(1, 1) == 1 漏掉了(1, 1)
+            for (int i = 1; i <= 50; i++) {
+                for (int j = i + 1; j <= 50; j++) {
+                    if (gcd(i, j) == 1) {
+                        hz[i].push_back(j);
+                        hz[j].push_back(i);
+                    }
+                }
+            }
+            */
+        }
+
         void dfs(vector<int>& ans, int i, int rank) {
             marked[i] = 1;
             int val = g[i].first;
-            ans[i] = getAns(val);
-            pre[val].push_back(rank);
-            pri[val].push_back(i);
+            ans[i] = !pre[val].empty() ? pre[val].back() : -1;
+            for (auto v : hz[val]) {
+                pre[v].push_back(i);
+            }
             for (auto& adj: g[i].second) {
                 if (!marked[adj]) {
                     dfs(ans, adj, rank + 1);
                 }
             }
-            pre[val].pop_back();
-            pri[val].pop_back();
+            for (auto v : hz[val]) {
+                pre[v].pop_back();
+            }
         }
 
         void solve(vector<int>& ans) {
             dfs(ans, 0, 0);
+        }
+    };
+    vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& edges) {
+        vector<int> ans(nums.size(), -1);
+        graph g(nums, edges);
+        g.solve(ans);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    class graph {
+     public:
+        int n;
+        vector<pair<int, vector<int>>> g;
+        // 每一个值出现的层级
+        pair<int, int> pre[51];
+        vector<int> hz[51];
+
+        graph(vector<int>& nums, vector<vector<int>>& edges) : n(nums.size()), g(n) {
+            for (int i = 0; i < n; i++) {
+                g[i] = {nums[i], {}};
+            }
+            for (auto& vec : edges) {
+                int from = vec[0], to = vec[1];
+                g[from].second.push_back(to);
+                g[to].second.push_back(from);
+            }
+            for (int i = 1; i <= 50; i++) {
+                for (int j = 1; j <= 50; j++) {
+                    if (gcd(i, j) == 1) {
+                        hz[i].push_back(j);
+                    }
+                }
+            }
+        }
+
+        int getAns(int val) {
+            int max_depth = 0;
+            int ans = -1;
+            for (auto v : hz[val]) {
+                auto [r, i] = pre[v];
+                if (r > max_depth) {
+                    max_depth = r;
+                    ans = i;
+                }
+            }
+            return ans;
+        }
+
+        void dfs(vector<int>& ans, int i, int fa, int rank) {
+            int val = g[i].first;
+            ans[i] = getAns(val);
+            auto tmp = pre[val];
+            pre[val] = {rank, i};
+            for (auto& adj: g[i].second) {
+                if (adj != fa) {
+                    dfs(ans, adj, i, rank + 1);
+                }
+            }
+            pre[val] = tmp;
+        }
+
+        void solve(vector<int>& ans) {
+            dfs(ans, 0, -1, 1);
         }
     };
     vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& edges) {
