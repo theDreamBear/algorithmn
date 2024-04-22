65cd52d50a6c980aeee495c73c914272b2b7da92
nichao <nichao@zuoyebang.com>
2024-04-22 13:01:01 +0800

    0

--- leetcode2/377.combination-sum-iv.cpp
+++ leetcode2/377.combination-sum-iv.cpp
@@ -28,7 +28,7 @@ using namespace std;
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned long long> dp(target + 1);
+        vector<unsigned int> dp(target + 1);
         dp[0] = 1;
         for (int i = 1; i <= target; i++) {
             for (auto v : nums) {
