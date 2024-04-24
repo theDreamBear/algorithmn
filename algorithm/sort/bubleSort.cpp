31c270782896099bca61c861dfba85612b42bc78
nichao <nichao@zuoyebang.com>
2024-04-24 10:30:15 +0800

    0

--- algorithm/sort/bubleSort.cpp
+++ algorithm/sort/bubleSort.cpp
@@ -40,6 +40,51 @@ class bubleSorter {
     }
 };
 
+
+void bubbleSort_common(vector<int>& data) {
+    for (int i = 0; i + 1 < data.size(); i++) {
+        for (int j = 0; j + i + 1 < data.size(); j++) {
+            if (data[j] > data[j + 1]) {
+                swap(data[j], data[j + 1]);
+            }
+        }
+    }
+}
+
+void bubbleSort_optimize_1(vector<int>& data) {
+    for (int i = 0; i + 1 < data.size(); i++) {
+        bool ordered = true;
+        for (int j = 0; j + i + 1 < data.size(); j++) {
+            if (data[j] > data[j + 1]) {
+                swap(data[j], data[j + 1]);
+                ordered = false;
+            }
+        }
+        if (ordered) {
+            break;
+        }
+    }
+}
+
+void bubbleSort_optimize_best(vector<int>& data) {
+    int lastSortedIndex = data.size();
+    // 1、[lastSortedIndex, +INF） 有序
+    bool ordered = false;
+    while (!ordered) {
+        ordered = true;
+        int  swapIndex = 0;
+        // 2、[swapIndex, lastSortedIndex) 本次有序
+        for (int j = 0; j + 1 < lastSortedIndex; j++) {
+            if (data[j] > data[j + 1]) {
+                swap(data[j], data[j + 1]);
+                swapIndex = j + 1;
+                ordered = false;
+            }
+        }
+        lastSortedIndex = swapIndex;
+    }
+}
+
 int main() {
     vector<int> res{1, 3, 2, 5, 2, 4, 9, 2, 1, 3};
     bubleSorter::sort(res);
