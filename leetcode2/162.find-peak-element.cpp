c389aca3348058059fc3f7f3c2ef8094a31faf27
nichao <nichao@zuoyebang.com>
2024-04-29 17:38:09 +0800

    0

---
+++ leetcode2/162.find-peak-element.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=cpp
+ * @lcpr version=30122
+ *
+ * [162] 寻找峰值
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        return nums[low] > nums[high] ? low : high;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,1,3,5,6,4]\n
+// @lcpr case=end
+
+ */
+
