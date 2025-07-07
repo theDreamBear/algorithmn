c08dc02bb49736ca9d6d0257df32eee9eefb018f
theDreamBear <512068167@qq.com>
2025-07-08 01:40:36 +0800

    0

---
+++ leetcode2/221.maximal-square.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=221 lang=cpp
+ * @lcpr version=30204
+ *
+ * [221] 最大正方形
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int ans = 0;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> sum(m + 1, vector<int>(n + 1));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] + matrix[i][j] - '0' - sum[i][j];
+                if (matrix[i][j] == '1') ans = 1;
+            }
+        }
+        auto cal = [&](int i, int j, int k) {
+            // (k - 1, k) 的矩形面积
+            // 右下角 i, j - 1
+            // 左上角 i - k + 1, j - k
+            return sum[i + 1][j] - sum[i + 1][j - k + 1] - sum[i - k + 1][j] + sum[i - k + 1][j - k + 1] == k * (k - 1) and
+                   sum[i][j + 1] - sum[i][j - k + 1] - sum[i - k + 1][j + 1] + sum[i - k + 1][j - k + 1] == k * (k - 1);
+        };
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != '1') continue;
+                for (int step = 1; step <= min(i, j) + 1; step++) {
+                    if (!cal(i, j, step)) break;
+                    else {
+                        ans = max(ans, step);
+                    }
+                }
+            }
+        }
+        return ans * ans;
+    }
+};
+
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["0","1"],["1","0"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["0"]]\n
+// @lcpr case=end
+
+ */
+
