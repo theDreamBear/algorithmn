4a874aafeff28f37558b4f749ba0223a1468f312
theDreamBear <512068167@qq.com>
2025-07-28 01:54:07 +0800

    0

--- leetcode2/70.climbing-stairs.cpp
+++ leetcode2/70.climbing-stairs.cpp
@@ -25,9 +25,41 @@ using namespace std;
 #include <vector>
 // @lcpr-template-end
 // @lc code=start
+using matrix = vector<vector<long long>>;
+
+matrix operator* (matrix& a, matrix& b) {
+    matrix c(a.size(), vector<long long>(a[0].size()));
+    for (int k = 0; k < a[0].size(); k++) {
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a[0].size(); j++) {
+                if (!a[i][k]) {
+                    continue;
+                }
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return c;
+}
+
+matrix makeMatrix(int a, int b) {
+    return matrix(a, vector<long long>(b));
+}
+
+matrix fastPow(matrix a, int n) {
+    matrix c = makeMatrix(a.size(), a[0].size());
+    for (int i = 0; i < a.size(); i++) c[i][i] = 1;
+    while (n) {
+        if (n & 1)  c = c * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return c;
+}
+
 class Solution {
 public:
-    int climbStairs(int n) {
+    int climbStairs1(int n) {
         if (n == 1) {
             return 1;
         }
@@ -39,6 +71,16 @@ public:
         }
         return p;
     }
+
+    int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        matrix a = {{1, 1}, {1, 0}};
+        auto c = fastPow(a, n - 1);
+        return c[0][0] + c[1][0];
+    }
+
 };
 // @lc code=end
 
