cbbdd9f08dcf82452daa502acc80f35f32793a78
nichao <nichao@zuoyebang.cc>
2025-08-14 00:32:52 +0800

    0

---
+++ leetcode2/1780.check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=1780 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1780] 判断一个数字是否可以表示成三的幂的和
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <string.h>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    using ll = long long;
+    bool checkPowersOfThree(int n) {
+        vector<ll> can;
+        ll i = 1;
+        while (i <= n) {
+            can.emplace_back(i);
+            i *= 3;
+        }
+        int size = can.size();
+        while (n and size > 0) {
+            auto idx = upper_bound(can.begin(), can.begin() + size, n) - can.begin() - 1;
+            n -= can[idx];
+            size = idx;
+        }
+        return n == 0;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 12\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 91\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 21\n
+// @lcpr case=end
+
+ */
+
